4d67aa90d5957392cc7ef449afed557a038cdf5a
WqyJh <781345688@qq.com>
2019-10-16 16:33:19 +0800

    WIP

--- test.sh
+++ test.sh
@@ -1,17 +1,56 @@
-curl -X POST -H 'Content-Type: application/json' http://localhost:9200/syslog-ng-app/_mapping/app_name -d '
+curl -X POST -H 'Content-Type: application/json' http://localhost:9200/syslog-ng-app/_mapping/app -d '
 {
-"properties": {
-"app_name": {
-                "type": "text",
-                "fielddata" : true,
-                "fields": {
-                    "keyword": {
-                        "type": "keyword",
-                        "ignore_above": 256
+    "properties": {
+        "app": {
+            "properties": {
+                "app_name": {
+                    "type": "text",
+                    "fielddata" : true,
+                    "fields": {
+                        "keyword": {
+                            "type": "keyword",
+                            "ignore_above": 256
+                        }
+                    }
+                },
+                "proc_name": {
+                    "type": "text",
+                    "fielddata" : true,
+                    "fields": {
+                        "keyword": {
+                            "type": "keyword",
+                            "ignore_above": 256
+                        }
+                    }
+                },
+                "pid": {
+                    "type": "long"
+                },
+                "create_time": {
+                    "type": "long"
+                },
+                "running_time": {
+                    "type": "long"
+                },
+                "mem_used": {
+                    "type": "long"
+                },
+                "timestamp": {
+                    "type": "date"
+                },
+                "host": {
+                    "type": "text",
+                    "fielddata" : true,
+                    "fields": {
+                        "keyword": {
+                            "type": "keyword",
+                            "ignore_above": 256
+                        }
                     }
                 }
             }
-}
+        }
+    }
 }'
 
 
